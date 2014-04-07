1a74f08d08b07327186986c85cbfa4a77a41bd11
Sean Skaar <sean.skaar@aa.com>
2014-04-07 00:08:18 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JacksonArch::Application.config.secret_key_base = 'ca1e8b23aa0d4ec105a241f42c69171bcf6cc5816be66b0cc9d11f9f74ff5c596186a58f76d8f4c66ed95ceb674513642360c0e8c18460d0100061d1d0315454'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JacksonArch::Application.config.session_store :cookie_store, key: '_JacksonArch_session'
