118d8ba37469c94d063b58397fac90784f75861d
Sean Skaar <sean.skaar@aa.com>
2014-05-08 11:51:02 -0500

    Trying again on the assets things

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+config.assets.initialize_on_precompile = false
+
+
 module JacksonArch
   class Application < Rails::Application
 
