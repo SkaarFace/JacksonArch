6206533dfc1a3b570653c1701ebbd2208e2d408f
Sean Skaar <sean.skaar@aa.com>
2014-05-08 13:00:05 -0500

    Starting to get pissed.

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,8 @@
 class Job < ActiveRecord::Base
   has_one :client
   belongs_to :client
-  has_one :job_type, :contract
+  has_one :job_type
+  has_one :contract
   has_one :job_category, through: :job_type 
   has_one :location, through: :client
 end
